7c2514f63d794d55e1271f52d885f654f6cca1d4
Bradley Ritschel <mcchicken442@gmail.com>
2024-04-28 06:32:41 +0545

    convert unity web requests to http

--- TelegramBot/LeaderboardCreatorBehaviour.cs
+++ TelegramBot/LeaderboardCreatorBehaviour.cs
@@ -7,31 +7,40 @@ using System.Net;
 using System.IO;
 using Microsoft.AspNet.SignalR.Json;
 using Microsoft.AspNetCore.Http;
+using System.Text;
+using System.Text.Json;
 
 
 public sealed class LeaderboardCreatorBehaviour
     {
         
-        private static HttpClient sharedClient = new()
+        private static HttpClient httpClient = new()
         {
             BaseAddress = new Uri(ConstantVariables.GetServerURL()),
         };
 
-        [Serializable]
-        private struct EntryResponse
+        private class EntryResponse : HttpResponseMessage
         {
-            public Entry[] entries;
+            public Entry[]? entries;
         }
-        
-        /*
-        internal static LeaderboardCreatorConfig Config =>
-            Resources.Load<LeaderboardCreatorConfig>("LeaderboardCreatorConfig");
 
-        private static string GetError(WebRequest request) =>
-            $"{request.responseCode}: {request.downloadHandler.text}";
-        */
-        
-        internal void Authorize(Action<string> callback)
+    private class SingleEntryResponse : HttpResponseMessage
+    {
+        public Entry entry;
+    }
+
+
+
+
+    /*
+    internal static LeaderboardCreatorConfig Config =>
+        Resources.Load<LeaderboardCreatorConfig>("LeaderboardCreatorConfig");
+
+    private static string GetError(WebRequest request) =>
+        $"{request.responseCode}: {request.downloadHandler.text}";
+    */
+
+    internal async void Authorize(Action<string> callback)
         {
             var loadedGuid = LoadGuid();
             if (!string.IsNullOrEmpty(loadedGuid))
@@ -39,8 +48,23 @@ public sealed class LeaderboardCreatorBehaviour
                 callback?.Invoke(loadedGuid);
                 return;
             }
-            
-            var request = WebRequest.Get(ConstantVariables.GetServerURL(Routes.Authorize));
+
+        using HttpResponseMessage response = await httpClient.GetAsync(ConstantVariables.GetServerURL(Routes.Authorize));
+
+        try
+        {
+            response.EnsureSuccessStatusCode();
+            var jsonResponse = await response.Content.ReadAsStringAsync();
+            var guid = jsonResponse.ToString(); 
+            //SaveGuid(guid);
+            callback?.Invoke(guid);
+        }
+        catch (Exception e)
+        {
+            callback?.Invoke(null);
+        }
+        /*
+        var request = WebRequest.Get(ConstantVariables.GetServerURL(Routes.Authorize));
             HandleRequest(request, isSuccessful =>
             {
                 if (!isSuccessful)
@@ -54,6 +78,7 @@ public sealed class LeaderboardCreatorBehaviour
                 //SaveGuid(guid);
                 callback?.Invoke(guid);
             });
+        */
         }
         
         internal void ResetAndAuthorize(Action<string> callback, Action onFinish)
@@ -68,42 +93,75 @@ public sealed class LeaderboardCreatorBehaviour
             Authorize(callback);
         }
         
-        internal void SendGetRequest(string url, Action<bool> callback, Action<string> errorCallback)
+        internal async void SendGetRequest(string url, Action<bool> callback, Action<string> errorCallback)
         {
-            var request = WebRequest.Get(url);
-            HandleRequest(request, isSuccessful =>
+        /*
+        var request = WebRequest.Get(url);
+        HandleRequest(request, isSuccessful =>
+        {
+            if (!isSuccessful)
             {
-                if (!isSuccessful)
-                {
-                    //HandleError(request);
-                    callback?.Invoke(false);
-                    //errorCallback?.Invoke(GetError(request));
-                    return;
-                }
-                callback?.Invoke(true);
-                LeaderboardCreator.Log("Successfully retrieved leaderboard data!");
-            });
-        }
-        
-        internal void SendGetRequest(string url, Action<int> callback, Action<string> errorCallback)
+                //HandleError(request);
+                callback?.Invoke(false);
+                //errorCallback?.Invoke(GetError(request));
+                return;
+            }
+            callback?.Invoke(true);
+            LeaderboardCreator.Log("Successfully retrieved leaderboard data!");
+        });
+        */
+        using HttpResponseMessage response = await httpClient.GetAsync(url);
+
+        try
         {
-            var request = WebRequest.Get(url);
-            HandleRequest(request, isSuccessful =>
+            response.EnsureSuccessStatusCode();
+            //var jsonResponse = await response.Content.ReadAsStringAsync();
+            callback?.Invoke(true);
+            LeaderboardCreator.Log("Successfully retrieved leaderboard data!");
+        }
+        catch (Exception e)
+        {
+            callback?.Invoke(false);
+            //errorCallback?.Invoke(GetError(request));
+        }
+    }
+        
+        internal async void SendGetRequest(string url, Action<int> callback, Action<string> errorCallback)
+        {
+        /*
+        var request = WebRequest.Get(url);
+        HandleRequest(request, isSuccessful =>
+        {
+            if (!isSuccessful)
             {
-                if (!isSuccessful)
-                {
-                    //HandleError(request);
-                    callback?.Invoke(0);
-                    //errorCallback?.Invoke(GetError(request));
-                    return;
-                }
-                callback?.Invoke(int.Parse(request.downloadHandler.text));
-                LeaderboardCreator.Log("Successfully retrieved leaderboard data!");
-            });
-        }
-        
-        internal void SendGetRequest(string url, Action<Entry> callback, Action<string> errorCallback)
+                //HandleError(request);
+                callback?.Invoke(0);
+                //errorCallback?.Invoke(GetError(request));
+                return;
+            }
+            callback?.Invoke(int.Parse(request.downloadHandler.text));
+            LeaderboardCreator.Log("Successfully retrieved leaderboard data!");
+        });
+        */
+        using HttpResponseMessage response = await httpClient.GetAsync(url);
+
+        try
         {
+            response.EnsureSuccessStatusCode();
+            //var jsonResponse = await response.Content.ReadAsStringAsync();
+            callback?.Invoke(Int32.Parse(await response.Content.ReadAsStringAsync()));
+            LeaderboardCreator.Log("Successfully retrieved leaderboard data!");
+        }
+        catch (Exception e)
+        {
+            callback?.Invoke(0);
+            //errorCallback?.Invoke(GetError(request));
+        }
+    }
+        
+        internal async void SendGetRequest(string url, Action<Entry> callback, Action<string> errorCallback)
+        {
+            /*
             var request = WebRequest.Get(url);
             HandleRequest(request, isSuccessful =>
             {
@@ -118,10 +176,27 @@ public sealed class LeaderboardCreatorBehaviour
                 callback?.Invoke(response);
                 LeaderboardCreator.Log("Successfully retrieved leaderboard data!");
             });
-        }
-        
-        internal void SendGetRequest(string url, Action<Entry[]> callback, Action<string> errorCallback)
+            */
+
+        using SingleEntryResponse response = (SingleEntryResponse) await httpClient.GetAsync(url);
+
+        try
         {
+            response.EnsureSuccessStatusCode();
+            //var jsonResponse = await response.Content.ReadAsStringAsync();
+            callback?.Invoke(response.entry);
+            LeaderboardCreator.Log("Successfully retrieved leaderboard data!");
+        }
+        catch (Exception e)
+        {
+            callback?.Invoke(new Entry());
+            //errorCallback?.Invoke(GetError(request));
+        }
+    }
+        
+        internal async void SendGetRequest(string url, Action<Entry[]> callback, Action<string> errorCallback)
+        {
+            /*
             var request = WebRequest.Get(url);
             HandleRequest(request, isSuccessful =>
             {
@@ -136,64 +211,111 @@ public sealed class LeaderboardCreatorBehaviour
                 callback?.Invoke(response.entries);
                 LeaderboardCreator.Log("Successfully retrieved leaderboard data!");
             });
-        }
-        
-        internal void SendPostRequest(string url, List<IMultipartFormSection> form, Action<bool> callback = null, Action<string> errorCallback = null)
-        {
-            var request = WebRequest.Post(url, form);
-            HandleRequest(request, callback, errorCallback);
-        }
-        
-        
-        private static IEnumerator HandleRequest(HttpRequest request, Action<bool> onComplete, Action<string> errorCallback = null)
-        {
+            */
 
-            yield return request.SendWebRequest();
+            using EntryResponse response = (EntryResponse)await httpClient.GetAsync(url);
 
-            if (request.responseCode != 200)
+            try
             {
-                onComplete.Invoke(false);
+                response.EnsureSuccessStatusCode();
+                //var jsonResponse = await response.Content.ReadAsStringAsync();
+                callback?.Invoke(response.entries);
+                LeaderboardCreator.Log("Successfully retrieved leaderboard data!");
+            }
+            catch (Exception e)
+            {
+                callback?.Invoke(Array.Empty<Entry>());
                 //errorCallback?.Invoke(GetError(request));
-                //request.downloadHandler.Dispose();
-                //request.Dispose();
-                yield break;
             }
 
-            onComplete.Invoke(true);
+        }
+        
+        internal async void  SendPostRequest(string url, List<IMultipartFormSection> form, Action<bool> callback = null, Action<string> errorCallback = null)
+        {
+            using StringContent jsonContent = new(
+                JsonSerializer.Serialize(new
+                {
+                    form = form
+                }),
+                Encoding.UTF8,
+                "application/json");
+
+            using HttpResponseMessage response = await httpClient.PostAsync(url, jsonContent);
+
+            try
+            {
+                response.EnsureSuccessStatusCode();
+                //var jsonResponse = await response.Content.ReadAsStringAsync();
+                callback?.Invoke(true);
+                LeaderboardCreator.Log("Successfully retrieved leaderboard data!");
+            }
+            catch (Exception e)
+            {
+                callback?.Invoke(false);
+                //errorCallback?.Invoke(GetError(request));
+            }
+        }
+        
+
+    /*
+    private static async IEnumerator HandleRequest(HttpRequest request, Action<bool> onComplete, Action<string> errorCallback = null)
+    {
+        if (request.Method == "GET")
+        {
+
+        }
+        else if (request.Method == "POST")
+        {
+
+        }
+
+        yield return request.SendWebRequest();
+
+        if (request.responseCode != 200)
+        {
+            onComplete.Invoke(false);
+            //errorCallback?.Invoke(GetError(request));
             //request.downloadHandler.Dispose();
             //request.Dispose();
+            yield break;
         }
-        /*
-        private static void HandleError(WebRequest request)
+
+        onComplete.Invoke(true);
+        //request.downloadHandler.Dispose();
+        //request.Dispose();
+    }
+    */
+    /*
+    private static void HandleError(WebRequest request)
+    {
+        var message = Enum.GetName(typeof(StatusCode), (StatusCode) request.responseCode);
+        message = string.IsNullOrEmpty(message) ? "Unknown" : message.SplitByUppercase();
+
+        var downloadHandler = request.downloadHandler;
+        var text = downloadHandler.text;
+        if (!string.IsNullOrEmpty(text))
+            message = $"{message}: {text}";
+        LeaderboardCreator.LogError(message);
+    }
+
+    private static void SaveGuid(string guid)
+    {
+        switch (Config.authSaveMode)
         {
-            var message = Enum.GetName(typeof(StatusCode), (StatusCode) request.responseCode);
-            message = string.IsNullOrEmpty(message) ? "Unknown" : message.SplitByUppercase();
-                
-            var downloadHandler = request.downloadHandler;
-            var text = downloadHandler.text;
-            if (!string.IsNullOrEmpty(text))
-                message = $"{message}: {text}";
-            LeaderboardCreator.LogError(message);
+            case AuthSaveMode.PlayerPrefs:
+                PlayerPrefs.SetString(GUID_KEY, guid);
+                PlayerPrefs.Save();
+                break;
+            case AuthSaveMode.PersistentDataPath:
+                var path = System.IO.Path.Combine(Application.persistentDataPath, Config.fileName);
+                if (string.IsNullOrEmpty(path))
+                    return;
+                System.IO.File.WriteAllText(path, guid);
+                break;
         }
-        
-        private static void SaveGuid(string guid)
-        {
-            switch (Config.authSaveMode)
-            {
-                case AuthSaveMode.PlayerPrefs:
-                    PlayerPrefs.SetString(GUID_KEY, guid);
-                    PlayerPrefs.Save();
-                    break;
-                case AuthSaveMode.PersistentDataPath:
-                    var path = System.IO.Path.Combine(Application.persistentDataPath, Config.fileName);
-                    if (string.IsNullOrEmpty(path))
-                        return;
-                    System.IO.File.WriteAllText(path, guid);
-                    break;
-            }
-        }
-        */
-        private static string LoadGuid()
+    }
+    */
+    private static string LoadGuid()
         {
             return "";
             /*
